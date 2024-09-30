ba9c8f415438aa6399f8fa9e34fc8b8118cedfb9
Ryan Skelton <ryanskelton2000@yahoo.com>
2024-10-01 00:23:41 +0100

    Add template files for cpp and java.

---
+++ templates/template.cpp
@@ -0,0 +1,18 @@
+/******************
+* Author: Ryan Skelton
+* file: template.cpp
+*******************/
+
+#define fori(x) for(int i = 0; i < x; i++)
+
+#include <iostream>
+
+void solve()
+{
+}
+
+int main(int argc, char * argv[])
+{
+    solve();
+    return 0;
+}
---
+++ templates/template.java
@@ -0,0 +1,19 @@
+/**********************************
+ * Author: Ryan Skelton
+ * file: template.java
+ **********************************/
+
+import java.util.Scanner;
+
+public class template
+{
+    public static void solve(Scanner in)
+    {
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        solve(in);
+    }
+}
