37e4b33e180918cdbc18a7519e1e3e4224fcd87a
Ryan Skelton <ryanskelton2000@yahoo.com>
2024-10-01 11:35:18 +0100

    Add template files for cpp and java.

--- templates/template.cpp
+++ templates/template.cpp
@@ -3,9 +3,39 @@
 * file: template.cpp
 *******************/
 
-#define fori(x) for(int i = 0; i < x; i++)
-
 #include <iostream>
+
+template<long N>
+struct fact
+{
+    static constexpr long value = N * fact<N - 1>::value;
+};
+
+template<>
+struct fact<0>
+{
+    static constexpr long value = 1;
+};
+
+template<long N>
+struct fib
+{
+    static constexpr long value = fib<N - 1>::value + fib<N - 2>::value;
+};
+
+template<>
+struct fib<1>
+{
+    static constexpr long value = 1;
+};
+
+template<>
+struct fib<0>
+{
+    static constexpr long value = 0;
+};
+
+#define fori(x) for(int i = 0; i < x; i++)
 
 void solve()
 {
