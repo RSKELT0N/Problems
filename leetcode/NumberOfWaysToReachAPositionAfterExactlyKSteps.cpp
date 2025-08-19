01f442405b94a01f4f3061f4be2981d4690037d5
RSKELT0N <ryanskelton2000@yahoo.com>
2025-08-19 23:18:49 +0100

    Add NumberOfWaysToReachAPositionAfterExactlyKSteps.cpp

--- leetcode/NumberOfWaysToReachAPositionAfterExactlyKSteps.cpp
+++ leetcode/NumberOfWaysToReachAPositionAfterExactlyKSteps.cpp
@@ -1,23 +1,18 @@
-struct pair_hash {
-    std::size_t operator()(const std::pair<int, int>& p) const {
-        return std::hash<int>()(p.first) ^ (std::hash<int>()(p.second) << 1);
-    }
-};
-
 class Solution {
 public:
-    std::unordered_map<std::pair<int, int>, int, pair_hash> dp;
+    std::vector<std::vector<int>> dp;
     int solve(int startPos, int endPos, int k)
     {
         if(startPos == endPos && k == 0) return 1;
         if(std::abs(startPos - endPos) > k) return 0;
-        if(dp.find({startPos, k}) != dp.end()) return dp[{startPos, k}];
+        if(dp[std::abs(startPos - endPos)][k] != -1) return dp[std::abs(startPos - endPos)][k];
 
-        dp[{startPos, k}] = (solve(startPos + 1, endPos, k - 1) + solve(startPos - 1, endPos, k - 1)) % static_cast<int>(1e9 + 7);
-        return dp[{startPos, k}];
+        dp[std::abs(startPos - endPos)][k] = (solve(startPos + 1, endPos, k - 1) + solve(startPos - 1, endPos, k - 1)) % static_cast<int>(1e9 + 7);
+        return dp[std::abs(startPos - endPos)][k];
     }
 
     int numberOfWays(int startPos, int endPos, int k) {
+        dp = std::vector<std::vector<int>>(k + 1, std::vector<int>(k + 1, -1));
         return solve(startPos, endPos, k);
     }
 };
